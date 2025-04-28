e2c1be5ecf64337ffb8372afa7f2e9f90e8bbe7a
Sanugie99 <Sanugie99@gmail.com>
2025-04-28 18:01:19 +0900

    2025.04.25

--- redux-react-app/src/Action.js
+++ redux-react-app/src/Action.js
@@ -1,21 +1,23 @@
-//액션(action): state를 변경하는 이벤트
+//액션(action) : state를 변경하는 이벤트
 
-//'INCREMENT'라는 타입의 액션을 반환하는 함수
+import { type } from "@testing-library/user-event/dist/type"
+
+//'INCREMENT' 라는 타입의 액션을 반환하는 함수
 export const increment = () => (
-    {type:'INCREMENT'} 
+    {type:'INCREMENT'}
     //액션의 타입을 'INCREMENT'로 정의
-    //이 액션은 리듀서에서 상태를 증가시키기 위한 신호로 사용됨
+    //이 액션은 리듀서에서 상탤르 증가시키기 위한 신호로 사용됨
 )
-//화살표함수에서 본문을 소괄호로 감싸면 그 안의 표현식이 암묵적으로 반환
+//화살표 함수에서 본문을 소괄호로 감싸면 그 안에 표현식이 암묵적으로 반환됨
 //() => ({...}) -> 안에 있는 값을 반환함
 
 export const decrement = () => (
     {type:'DECREMENT'}
 )
-//이 함수들은 단순히 특정 타입을 가진 객체를 반환하는데, 이 객체는 REDUX
-//리듀서에서 상태를 변경하기 위한 신호로 사용이 된다.
-//type은 Redux 리듀서가 상태를 어떻게 변경하지 결정하는 기준이 된다.
-//액션은 상태 변경을 하기 위한 이벤트이다.
+//이 함수들은 단순히 특정 타입을 가진 객체를 반환하는데
+//이 객체는 REDUX 리듀서에서 상태를 변경하기 위한 신호로 사용이 된다.
+//type은 Redux 리뷰서가 상태를 어떻게 변경할지 결정하는 기준이 된다.
+//액션은 상태 변경을 하기 위한 이벤트 이다.
 
 export const setMessage = (text) => ({
     type:'SET_MESSAGE',
@@ -30,7 +32,3 @@ export const setUser = (userInfo) => ({
 export const logout = () => ({
     type:'LOGOUT'
 });
-
-
-
-
